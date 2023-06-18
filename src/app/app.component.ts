ba2641b2fe89cf9bfcd2aadef4b27c93fd5ba31f
gustavosalm <gustavosalmeida9821@gmail.com>
2023-06-18 16:49:50 -0300

    new build with adjustments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-zionSite';
+  states = [false, false];
 
   openFaqCard(e: any) {
     if(e.classList.contains('hidden')){
@@ -16,4 +17,21 @@ export class AppComponent {
       e.classList.add('hidden');
     }
   }
+
+  changeDropdownState(ind: number) {
+    let dds = document.getElementsByClassName('dropdownObj');
+    let ddsImg = document.getElementsByClassName('dropdownArrowImg');
+
+    if(this.states[ind]){
+      dds[ind].classList.add('collapsed');
+      ddsImg[ind].classList.add('collapsed');
+    }
+    else{
+      dds[ind].classList.remove('collapsed');
+      ddsImg[ind].classList.remove('collapsed');
+    }
+
+    this.states[ind] = !this.states[ind];
+  }
+
 }
